4b8b2cb90b6e0cbfebef33b931103c9b9a310fc4
S3N4T0R <121706460+S3N4T0R-0X0@users.noreply.github.com>
2024-09-30 19:52:07 -0400

    Update Bear.py

--- Bear.py
+++ Bear.py
@@ -22,32 +22,31 @@ def display_banner():
 # Function to run selected cloud storage script
 def run_script(choice):
     try:
-    
         if choice == 1:
-            subprocess.run(["python3", "Profiles/general-beacon.py"], check=True)
-        if choice == 2:
-            subprocess.run(["python3", "Profiles/OneDrive.py"], check=True)
+            subprocess.run(["Profiles/OneDrive.o"], check=True)
+        elif choice == 2:
+            subprocess.run(["Profiles/GoogleDrive.o"], check=True)
         elif choice == 3:
-            subprocess.run(["python3", "Profiles/Dropbox.py"], check=True)
+            subprocess.run(["Profiles/Dropbox.o"], check=True)
         elif choice == 4:
-            subprocess.run(["php", "Profiles/XOR.php"], check=True)
+            subprocess.run(["Profiles/Discord.o"], check=True)
         elif choice == 5:
-            subprocess.run(["perl", "Profiles/DES.pl"], check=True)
+            subprocess.run(["Profiles/Backdoor-C2.o"], check=True)
         elif choice == 6:
-            subprocess.run(["python3", "Profiles/Backdoor-C2.py"], check=True)
+            subprocess.run(["Profiles/XOR.o"], check=True)
         elif choice == 7:
-            subprocess.run(["python3", "Profiles/GoogleDrive.py"], check=True)
+            subprocess.run(["Profiles/RSA.o"], check=True)
         elif choice == 8:
-            subprocess.run(["php", "Profiles/RSA.php"], check=True)   
+            subprocess.run(["Profiles/DES.o"], check=True)
         elif choice == 9:
-            subprocess.run(["python3", "Profiles/Discord.py"], check=True) 
-        elif choice == 10:
             print("\033[91m waiting you for the next attack, Goodbye!\033[0m")
             return True
         else:
-            print("Invalid choice. Please select a number between 1 and 10")
+            print("Invalid choice. Please select a number between 1 and 9")
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while running the script: {e}")
+    except PermissionError:
+        print("Permission denied. Please check the file permissions.")
     return False
 
 # Main function
@@ -55,20 +54,18 @@ def main():
     while True:
         display_banner()
         print("+---------------------------------------------------------------------------------------------------+")
-        print("| 1. General Beacon                                                                                 |")
-        print("+---------------------------------------------------------------------------------------------------+")
         print("| \033[93mProfiles                       Encryptions                         Actors                Country  \033[0m|")
         print("+---------------------------------------------------------------------------------------------------+")
-        print("| 2. OneDrive-API       |        AES encryption         |          Fancy Bear         |      RUSSIA |")
+        print("| 1. OneDrive-API       |        AES encryption         |          Fancy Bear         |      RUSSIA |")
+        print("| 2. GoogleDrive-API    |        RC4 encryption         |          Gossamer Bear      |      RUSSIA |")
         print("| 3. Dropbox-API        |        AES encryption         |          Cozy Bear          |      RUSSIA |")
-        print("| 4. PHP-backend        |        XOR encryption         |          Energetic Bear     |      RUSSIA |")
-        print("| 5. Perl 8-bytes       |        DES encryption         |          Primitive Bear     |      RUSSIA |")
-        print("| 6. Backdoor-Listener  |        TLS encryption         |          Venomous Bear      |      RUSSIA |")
-        print("| 7. GoogleDrive-API    |        RC4 encryption         |          Gossamer Bear      |      RUSSIA |")
-        print("| 8. Private Key-2048   |        RSA encryption         |          Voodoo Bear        |      RUSSIA |")
-        print("| 9. Discord-Bot        |       ChaCha encryption       |          Ember Bear         |      RUSSIA |")
+        print("| 4. Discord-Bot        |       ChaCha encryption       |          Ember Bear         |      RUSSIA |")
+        print("| 5. Backdoor-Listener  |        TLS encryption         |          Venomous Bear      |      RUSSIA |")
+        print("| 6. XOR-backend        |        XOR encryption         |          Energetic Bear     |      RUSSIA |")
+        print("| 7. Private Key-2048   |        RSA encryption         |          Voodoo Bear        |      RUSSIA |")
+        print("| 8. DES-8-bytes        |        DES encryption         |          Primitive Bear     |      RUSSIA |")
         print("+---------------------------------------------------------------------------------------------------+")
-        print("|10. Exit                                                                                           |")
+        print("| 9. Exit                                                                                           |")
         print("+---------------------------------------------------------------------------------------------------+")
         try:
             choice = int(input("\n[\033[93m*\033[0m] \033[91mBEAR >> \033[0m"))
